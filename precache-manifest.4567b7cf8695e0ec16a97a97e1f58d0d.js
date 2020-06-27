fdb743aa5984314f538ff56a9b7b6e4e574d72ac
a-tonkikh <tonkikhalina@yahoo.com>
2020-06-27 19:06:04 +0300

    Updates

---
+++ precache-manifest.4567b7cf8695e0ec16a97a97e1f58d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf225122a05c9eafea2e62aa3eccd51",
+    "url": "/manometer/index.html"
+  },
+  {
+    "revision": "768f71ab02f2f664e7ea",
+    "url": "/manometer/static/css/main.8be7f1de.chunk.css"
+  },
+  {
+    "revision": "b0780b591045dedc6f71",
+    "url": "/manometer/static/js/2.6a58b9bd.chunk.js"
+  },
+  {
+    "revision": "c60a93b7e8627dc76fb931ee62493de0",
+    "url": "/manometer/static/js/2.6a58b9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768f71ab02f2f664e7ea",
+    "url": "/manometer/static/js/main.ea6e916e.chunk.js"
+  },
+  {
+    "revision": "9c368e617267f1d6053a",
+    "url": "/manometer/static/js/runtime-main.110a14a6.js"
+  }
+]);
